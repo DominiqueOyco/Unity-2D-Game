066bc2e73cff05cde54ff7eb9a99cbb3352d1e7d
Dominique Oyco <dominickoyco@gmail.com>
2019-06-01 18:20:02 -0700

    update

--- Ruby 2D Game/Assets/Scripts/RubyController.cs
+++ Ruby 2D Game/Assets/Scripts/RubyController.cs
@@ -16,10 +16,13 @@ public class RubyController : MonoBehaviour
 
     Rigidbody2D rigidbody2d;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         currentHealth = maxHealth;
         currentHealth = 100;
     }
@@ -27,13 +30,33 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //float horizontal = Input.GetAxis("Horizontal");
+        //float vertical = Input.GetAxis("Vertical");
+
+        //Vector2 position = rigidbody2d.position;
+        //position.x = position.x + speed * horizontal * Time.deltaTime;
+        //position.y = position.y + speed * vertical * Time.deltaTime;
+
+
+        //rigidbody2d.MovePosition(position);
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
+        Vector2 move = new Vector2(horizontal, vertical);
 
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+        Vector2 position = rigidbody2d.position;
+
+        position = position + move * speed * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
 
@@ -60,5 +83,15 @@ public class RubyController : MonoBehaviour
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
 
         Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
     }
 }
